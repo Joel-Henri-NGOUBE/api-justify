31bdac25abe7efd8d1fc32f8d8797c188efb4563
Joel-Henri-NGOUBE <ebosqua.edimo@gmail.com>
2025-10-16 23:38:12 +0200

    Justification modifications to represent better the spaces addition

--- src/justification/justify.ts
+++ src/justification/justify.ts
@@ -13,8 +13,8 @@ export function justify(text: string, wordsPerLine: number = 80): string{
         let lastSameLineWordIndex: number = 0
         let currentCharCount: number = 0
         let lines = []
-        for(let i = 0; i <= wordsNumber; i++){
-            if(currentCharCount + words[i].length <= wordsPerLine){
+        for(let i = 0; i < wordsNumber; i++){
+            if(currentCharCount + words[i].length + 1 <= wordsPerLine){
                 // The current count of characters on the line is incremented with the following word number of chars and with 1 for the space that will follow the char
                 currentCharCount += words[i].length + 1
                 lastSameLineWordIndex = i
@@ -25,19 +25,31 @@ export function justify(text: string, wordsPerLine: number = 80): string{
                 const subWordsOfCurrentLine: Array<string> = words.filter((_, index) => index <= lastSameLineWordIndex && index >= firstSameLineWordIndex)
                 let spacesToInsert: number = wordsPerLine - currentCharCount
                 let subWordsOfCurrentLineLength: number = subWordsOfCurrentLine.length
+                const wordsWithMandatorySpaces: Array<string> = subWordsOfCurrentLine.map((word, index) => 
+                    (index !== subWordsOfCurrentLineLength - 1)
+                    ? word + " " 
+                    : word)
+                const currentNumberOfSpacesAdded = subWordsOfCurrentLineLength - 1
                 while(spacesToInsert > 0){
-                    const areSpacesMoreNumerousThanWordsInCurrentLine: boolean = spacesToInsert > subWordsOfCurrentLineLength
-                    const wordsWithSpaces: Array<string> = subWordsOfCurrentLine.map((word, index) => 
+                    const areSpacesMoreNumerousThanWordsInCurrentLine: boolean = spacesToInsert - currentNumberOfSpacesAdded > subWordsOfCurrentLineLength
+                    const wordsWithAdditionnalSpaces: Array<string> = wordsWithMandatorySpaces.map((word, index) => 
                         (areSpacesMoreNumerousThanWordsInCurrentLine && (index !== subWordsOfCurrentLineLength - 1)) 
                         ? word + " " 
-                        : (index <= spacesToInsert - 1 
+                        : (index < spacesToInsert 
                             ? word + " " 
                             : word))
-                    lines.push(wordsWithSpaces.join(""))
+                    // const wordsWithSpaces: Array<string> = subWordsOfCurrentLine.map((word, index) => 
+                    //     (areSpacesMoreNumerousThanWordsInCurrentLine && (index !== subWordsOfCurrentLineLength - 1)) 
+                    //     ? word + " " 
+                    //     : (index <= spacesToInsert - 1 
+                    //         ? word + " " 
+                    //         : word))
+                    lines.push(wordsWithAdditionnalSpaces.join(""))
                     spacesToInsert -= subWordsOfCurrentLineLength
-                    // console.log(i)
+                    // console.log(lines)
                 }
-                console.log(lines)
+                // lines.push(wordsWithMandatorySpaces.join(""))
+                // console.log(lines.length)
                 firstSameLineWordIndex = i + 1
                 currentCharCount = 0
             }     
